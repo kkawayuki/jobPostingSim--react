4ab833d23176ef0b72f4bf8e2d177fbfe042f860
Kent K <kyukiokawa@gmail.com>
2026-03-02 13:04:52 -0800

    prep for deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://github.com/kkawayuki/jobPostingSim--react",
 })
+
+//need to add base location of file hosting for vite to find/build properly
